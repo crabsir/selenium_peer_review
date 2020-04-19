598fda12521de6b1a53fe1cc5537e2135b9905fe
Crabsir <60735124+crabsir@users.noreply.github.com>
2020-04-19 16:38:40 +0300

    Lesson 3.6 step 9 uploaded

---
+++ test_items.py
@@ -0,0 +1,13 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
+def test_options(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+
+    try:
+        add_to_basket = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    except NoSuchElementException:
+        add_to_basket = False
+
+    assert add_to_basket, "'Add to basket' button not found!"
